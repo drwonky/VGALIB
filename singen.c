1714abac2d5b3a908e3f8477e1ea970b04b71885
Perry Harrington <pedward@apsoft.com>
2018-12-27 00:33:29 -0800

    Added additional assets needed for build/run

---
+++ singen.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+void main(void)
+{
+	double sinma;
+	double cosma;
+	int angle;
+
+	printf("#ifndef SINCOS_H\n#define SINCOS_H\n");
+	printf("int sindeg[] = {\n");
+	for (angle=0;angle<361;angle++) {
+		sinma = sin(-angle*M_PI/180);
+
+		printf("%ld%s // %d %f\n", (long)(sinma*10000),angle<360?",":"",angle,sinma);
+	}
+
+	printf("};\n");
+	printf("int cosdeg[] = {\n");
+	for (angle=0;angle<361;angle++) {
+		cosma = cos(-angle*M_PI/180);
+
+		printf("%ld%s // %d %f\n",(long)(cosma*10000),angle<360?",":"",angle,cosma);
+	}
+	printf("};\n#endif\n");
+}
