f9073ccc3807cca4e7cc39221b5029b901bd076c
Perry Harrington <pedward@apsoft.com>
2020-02-24 00:14:59 -0800

    Updated basic CGA stub functionality

--- cga.cpp
+++ cga.cpp
@@ -6,7 +6,7 @@
 #include "image.h"
 #include "types.h"
 #include "memory.h"
-#include "vga.h"
+#include "cga.h"
 
 #ifdef __GNUC__
 #include <iostream>
@@ -14,87 +14,25 @@
 using namespace std;
 #endif
 
-const unsigned char vga::gtable[]={0x35,0x2d,0x2e,0x07,
-			0x5b,0x02,0x57,0x57,
-			0x02,0x03,0x00,0x00};
-const unsigned char vga::ttable[]={0x61,0x50,0x52,0x0f,
-			0x19,0x06,0x19,0x19,
-			0x02,0x0d,0x0b,0x0c};
-
 /* mode, width, height, bytes_per_line, base_addr, bpp, Bpp, colors, vsync_reg, scale */
-const vga::video_mode vga::video_modes[] = {
- {TEXT,80,25,80,0xb800,1,8,0xF,0x3da,0},
- {VGAMONO,640,480,80,0xa000,1,1,0x1,0x3da,0},
- {VGAHI,640,480,80,0xa000,4,1,0xF,0x3da,0},
- {VGALO,320,200,320,0xa000,1,8,0xFF,0x3da,2},
- {X16,160,100,160,0xb800,1,8,0xF,0x3da,4},
- {SDLVGALO,320,200,320,0xa000,1,8,0xFF,0x3da,2},
- {SDLX16,160,100,160,0xb800,1,8,0xF,0x3da,4},
- {MDA,80,25,80,0xb000,1,8,0xF,0x3ba,0}
+const adapter::video_mode cga::video_modes[] = {
+ {TEXT,80,25,80,0xb800,8,8,0xF,0x3da,0},
+ {CGALO,320,200,80,0xb800,2,8,0xF,0x3da,0},
+ {CGAHI,640,200,80,0xb800,1,8,0xF,0x3da,0},
+ {X16,160,100,160,0xb800,1,8,0xF,0x3da,4}
 };
 
-bool vga::SDLonce = false;
-
-vga::vga(void)
+cga::cga(void)
 {
-#ifdef SDL
-	_window=NULL;
-	_renderer=NULL;
-	_render=NULL;
-	_texture=NULL;
-	_sdlscale=0;
-	_screen=NULL;
-#endif
-
-	colors=0;
-	_width=0;
-	_height=0;
-	_palette=NULL;
-	_palette_size=0;
-	_cur_palette=palette::NONE;
-	_row_bytes=0;
-	bpp=0;
-	Bpp=0;
-	SR=0;
-	planes=0;
-	_buffer=NULL;
-	buf_size=0;
-
-#ifndef SDL
-	getmode();
-
-	if (vmode == MDA) {
-		card = MONO;
-	} else {
-		card = COLOR;
-	}
-#else
-	vmode = NONE;
-
-	if (!SDLonce) {
-		SDLonce=true;
-		SDL_Init(SDL_INIT_VIDEO|SDL_INIT_EVENTS);
-	}
-
-	card=COLOR;
-#endif
-
+	_savedvmode = getmode();
 }
 
-vga::~vga(void)
+cga::~cga(void)
 {
-#ifdef SDL
-	if (SDLonce) {
-		if (_render) SDL_FreeSurface(_render);
-		if (_screen) SDL_FreeSurface(_screen);
-		if (_texture) SDL_DestroyTexture(_texture);
-		SDL_DestroyWindow( _window );
-		SDL_Quit();
-	}
-#endif
+	setmode(_savedvmode);
 }
 
-ptr_t vga::allocate_screen_buffer()
+ptr_t cga::allocate_screen_buffer()
 {
 	ptr_t buf = new unsigned far char [buf_size];
 	if (buf == NULL) {
@@ -104,26 +42,23 @@ ptr_t vga::allocate_screen_buffer()
 	return buf;
 }
 
-bool vga::setup(void)
+bool cga::setup(void)
 {
 	for (unsigned long int i=0;i<sizeof(video_modes);i++) {
-		if (video_modes[i].mode == vmode) {
+		if (video_modes[i].mode == _vmode) {
 			_row_bytes = video_modes[i].bytes;
 			_width = video_modes[i].x;
 			_height = video_modes[i].y;
 			bpp = video_modes[i].bpp;
-			Bpp = bpp/8;
+			Bpp = 8/bpp;
 			planes = video_modes[i].planes;
 			colors = video_modes[i].colors;
 			SR = video_modes[i].sr;
-			buf_size = _width*_height;
-#ifndef SDL
+			buf_size = _width*_height/Bpp;
+
+#ifdef __BORLANDC__
 			_buffer = (unsigned char far *) MK_FP(video_modes[i].fp,0);
 			screen.size(_width,_height);
-#else
-			_sdlscale = video_modes[i].scale;
-			_render = SDL_CreateRGBSurface(0,_width,_height,8,0,0,0,0);
-			screen = canvas(_width,_height,(ptr_t)_render->pixels);
 #endif
 			return true;
 		}
@@ -131,200 +66,80 @@ bool vga::setup(void)
 	return false;
 }
 
-bool vga::setpalette(palette::pal_type pal)
+void cga::setmode(Mode mode)
 {
-	_palette_size=palette::palettes[pal].palette_entries;
-	_cur_palette=pal;
-	_palette=new palette::pal_t[_palette_size];
-
-	if (_palette == NULL) return false;
-
-	memcpy(_palette,palette::palettes[pal].pal,sizeof(palette::pal_t)*_palette_size);
-
-#ifdef SDL
-	SDL_Palette* sdl_palette = SDL_AllocPalette(_palette_size);
-
-	for(int i=0;i<_palette_size;i++) {
-		sdl_palette->colors[i].r=_palette[i].r;
-		sdl_palette->colors[i].g=_palette[i].g;
-		sdl_palette->colors[i].b=_palette[i].b;
-	}
-
-	SDL_SetSurfacePalette(_render,sdl_palette);
-//	SDL_SetSurfacePalette(_spritescreen,sdl_palette);
-
-	SDL_FreePalette(sdl_palette);
-#endif
-
-	return true;
-}
-
-bool vga::graphmode(void)
-{
-#ifndef SDL
-	vmode = VGAMONO;
-	_AL=(unsigned char) VGAMONO;
-	_AH=0;
-	geninterrupt(0x10);
-	return setup();
-#else
-	return true;
-#endif
-}
-
-bool vga::sdlmode(Vgamode mode)
-{
-#ifdef SDL
-	vmode=mode;
-	setup();
-
-	_window = SDL_CreateWindow("VGALIB",SDL_WINDOWPOS_UNDEFINED,SDL_WINDOWPOS_UNDEFINED,_width*_sdlscale,_height*_sdlscale,SDL_WINDOW_SHOWN|SDL_WINDOW_RESIZABLE);
-	_renderer = SDL_CreateRenderer(_window, -1, SDL_RENDERER_ACCELERATED|SDL_RENDERER_PRESENTVSYNC);
-	SDL_SetHintWithPriority(SDL_HINT_RENDER_VSYNC, "1", SDL_HINT_OVERRIDE);
-	SDL_RenderClear(_renderer);
-	SDL_RenderSetLogicalSize(_renderer, _width, _height);
-	_texture = SDL_CreateTexture(_renderer,
-			SDL_PIXELFORMAT_ARGB8888,
-            SDL_TEXTUREACCESS_STREAMING,
-            _width, _height);
-
-	SDL_SetRenderTarget(_renderer, _texture);
-
-	_screen = SDL_CreateRGBSurface(0, _width, _height, 32, 0, 0, 0, 0);
-
-	return false;
-#else
-	mode=mode; // silence annoying compile warning
-	return true;
-#endif
-}
-
-bool vga::graphmode(Vgamode mode)
-{
-#ifdef SDL
-	switch (mode) {
-		case VGALO:
-		case SDLVGALO:
-			setpalette(palette::VGA_PAL);
-			return sdlmode(mode);
-		case X16:
-		case SDLX16:
-			setpalette(palette::CGA_PAL);
-			return sdlmode(mode);
-		default:
-			return false;
-	}
-#else
-	switch (mode) {
-		case MDA:
-			return mdamode();
-		case TEXT:
-			return textmode();
-		case X16:
-			setpalette(palette::CGA_PAL);
-			if (card == MONO) return mdamode();
-			return x16mode();
-		case VGALO:
-			setpalette(palette::VGA_PAL);
-			break;
-	}
-
-	vmode=mode;
+#ifdef __BORLANDC__
 	_AL=(unsigned char) mode;
 	_AH=0;
 	geninterrupt(0x10);
-	return setup();
 #endif
 }
 
-bool vga::textmode(void)
+bool cga::graphmode(Mode mode)
 {
-	if (card == MONO) {
-		vmode = MDA;
-	} else {
-		vmode = TEXT;
+	switch (mode) {
+		case TEXT:
+			return textmode();
+		case X16:
+			setpalette(palette::TEXT_PAL);
+			return x16mode();
+		case CGALO:
+			setpalette(palette::CGA1_PAL);
+			break;
 	}
 
-#ifndef SDL
-	_AL=(unsigned char) vmode;
-	_AH=0;
-	geninterrupt(0x10);
+	_vmode=mode;
+
+	setmode(_vmode);
+	return setup();
+}
+
+bool cga::textmode(void)
+{
+#ifdef __BORLANDC__
+	setmode(3);
 	return setup();
 #else
 	return true;
 #endif
 }
 
-bool vga::detectMDA(void)
+
+bool cga::x16mode(void)
 {
-#ifndef SDL
-	unsigned int i;
-
-	for (i=0;i<32768;i++) {
-		if ((inportb(CRTCmd) & 0x80) == 0) break;
-	}
-
-	return i == 32768 ? false : true;
-#else
-	return false;
-#endif
-}
-
-bool vga::mdamode(void)
-{
-#ifndef SDL
-	_AL=(unsigned char) MDA;
-	_AH=0;
-	geninterrupt(0x10);
-
-	outportb(CRTCma, 0);  // disable screen
-
-	write_crtc(CRTCmb,0x0a,0x10);  // turn off cursor
-
-	outportb(CRTCma, 8);  // enable screen, disable blink
-
-	vmode=MDA;
-
-	return setup();
-#else
-	return false;
-#endif
-}
-
-bool vga::x16mode(void)
-{
-#ifndef SDL
+#ifdef __BORLANDC__
 	unsigned char status;
 
-	_AL=(unsigned char) TEXT;
-	_AH=0;
-	geninterrupt(0x10);
+	setmode(TEXT);
 
+    /* set mode control register for 80x25 text mode and disable video output */
+    outportb(CRTCa, 1);
 
-	//disable VGA blink
-	status=inportb(0x3da);
-	outportb(0x3c0,0x30);
-	status=inportb(0x3c1);
+    /*
+            These settings put the 6845 into "graphics" mode without actually
+            switching the CGA controller into graphics mode.  The register
+            values are directly copied from CGA graphics mode register
+            settings.  The 6845 does not directly display graphics, the
+            6845 only generates addresses and sync signals, the CGA
+            attribute controller either displays character ROM data or color
+            pixel data, this is external to the 6845 and keeps the CGA card
+            in text mode.
+            ref: HELPPC
+    */
 
-	if (status != 0xFF) {  // VGA presumed, turn 80x50 into 80x100
+    /* set vert total lines to 127 */
+    write_crtc(CRTCb,0x04,0x7f);
+    /* set vert displayed char rows to 100 */
+    write_crtc(CRTCb,0x06,0x64);
+    /* set vert sync position to 112 */
+    write_crtc(CRTCb,0x07,0x70);
+    /* set char scan line count to 1 */
+    write_crtc(CRTCb,0x09,0x01);
 
-		status&=0xf7;
-		outportb(0x3c0,status);
-		write_crtc(CRTCb,0x09,0x03);
+    /* re-enable the video output in 80x25 text mode */
+    outportb(CRTCa, 9);
 
-	} else { // CGA assumed
-
-		outportb(CRTCa, 1);
-
-		write_crtc(CRTCb,0x04,0x7f);
-		write_crtc(CRTCb,0x06,0x64);
-		write_crtc(CRTCb,0x07,0x70);
-		write_crtc(CRTCb,0x09,0x01);
-
-		outportb(CRTCa, 9);
-	}
-
-	vmode=X16;
+	_vmode=X16;
 
 	return setup();
 #else
@@ -332,88 +147,42 @@ bool vga::x16mode(void)
 #endif
 }
 
-void vga::write_crtc(unsigned int port, unsigned char reg, unsigned char val)
+void cga::write_crtc(unsigned int port, unsigned char reg, unsigned char val)
 {
-#ifndef SDL
+#ifdef __BORLANDC__
 	outportb(port, reg);
 	outportb(port+1,val);
 #endif
 }
 
-vga::Vgamode vga::getmode(void)
+adapter::Mode cga::getmode(void)
 {
-#ifndef SDL
+#ifdef __BORLANDC__
 	_AX=0x0f00;
 	geninterrupt(0x10);
-	vmode=(Vgamode)_AL;
-	return (vmode);
+	_vmode=(Mode)_AL;
+	return (_vmode);
 #else
-	return vmode;
+	return _vmode;
 #endif
 }
 
-void vga::setpixel(int x, int y, unsigned char visible)
-{
-	static unsigned int plane_offset=0;
-	static unsigned char temp;
-	static unsigned char bit;
-
-	visible&=colors;
-
-	switch (vmode) {
-		case VGAMONO:
-			plane_offset=(y*_row_bytes)+(x>>3);
-			bit=(7-(x&7));
-			temp=screen._buffer[plane_offset];
-			temp=temp&(255^(1<<bit));
-			temp=temp|(visible<<bit);
-			screen._buffer[plane_offset]=temp;
-			break;
-		case VGAHI:
-
-			break;
-		default:
-			plane_offset=(y*_row_bytes)+(x);
-			screen._buffer[plane_offset]=visible;
-			break;
-	}
-}
-
-unsigned char vga::getpixel(int x, int y)
-{
-	static unsigned int plane_offset=0;
-
-	switch (vmode) {
-		case VGAMONO:
-			plane_offset=(y*_row_bytes)+(x>>3);
-			return((screen._buffer[plane_offset]&(7-(x&7)))>>(7-(x&7)));
-		case VGAHI:
-			break;
-		case TEXT:
-			plane_offset=(y*_row_bytes)+(x<<1);
-			return(screen._buffer[plane_offset]);
-		default:
-			plane_offset=(y*_row_bytes)+(x);
-			return(screen._buffer[plane_offset]);
-	}
-	return 0;
-}
-
-void vga::cls(void)
+void cga::cls(void)
 {
 	screen.clear();
+	update();
 }
 
-void vga::update(void)
+void cga::update(void)
 {
 	vsync();
 	translate(screen._buffer);
 }
 
-void vga::translate(unsigned char far *src)
+void cga::translate(unsigned char far *src)
 {
-#ifndef SDL
-	switch (vmode) {
+#ifdef __BORLANDC__
+	switch (_vmode) {
 		case MDA:
 		case TEXT:
 			_CX=buf_size>>1;
@@ -469,30 +238,14 @@ void vga::translate(unsigned char far *src)
 			memory::blit(_buffer,src,buf_size);
 			break;
 	}
-#else
-	void *pixels;
-	int pitch;
-
-	SDL_BlitSurface(_render, NULL, _screen, NULL);
-
-	SDL_LockTexture(_texture, NULL, &pixels, &pitch);
-
-	SDL_ConvertPixels(_screen->w, _screen->h, _screen->format->format,
-			_screen->pixels, _screen->pitch, SDL_PIXELFORMAT_ARGB8888, pixels,
-			pitch);
-	SDL_UnlockTexture(_texture);
-
-	SDL_RenderCopy(_renderer, _texture, NULL, NULL);
-	SDL_RenderPresent(_renderer);
 #endif
 }
 
-void vga::vsync(void)
+void cga::vsync(void)
 {
-#ifndef SDL
+#ifdef __BORLANDC__
 	unsigned char p;
 
-	if (card == COLOR) {
 		do {
 			p=inportb(SR);
 		} while (p & 8);
@@ -500,115 +253,18 @@ void vga::vsync(void)
 		do {
 			p=inportb(SR);
 		} while (!(p & 8));
-	} else {
-		do {
-			p=inportb(SR);
-		} while (p & 0x80);
-
-		do {
-			p=inportb(SR);
-		} while (!(p & 0x80));
-	}
 #endif
 }
 
-void vga::writef(int col, int row, int color, char *format, ...)
-/* Prints a string in text video memory at a selected location in a color */
+bool cga::setpalette(palette::pal_type pal)
 {
-	va_list arg_ptr;
-	char output[81];
-	int len,start,i;
-
-	va_start(arg_ptr, format);
-	len = vsprintf(output, format, arg_ptr);
-	output[len] = 0;
-	start=row*160+(col<<1);
-	i=0;
-	while(i<len-1) {
-		_buffer[start]=output[i];
-		_buffer[start+1]=color&0x0F;
-		start+=2;
-		i++;
-	}
-} /* writef */
-
-bool vga::kbhit(void)
-{
-#ifdef SDL
-	if (SDL_PollEvent(&_event)) {
-		switch (_event.type) {
-		case SDL_QUIT:
-		case SDL_KEYDOWN:
-			std::cout<<"caught event "<<std::hex<<_event.type<<" keysym "<<_event.key.keysym.sym<<"("<<std::dec<<_event.key.keysym.sym<<")"<<std::endl;
-			return true;
-			break;
-		}
-	}
-	return false;
-#else
-	return kbhit();
-#endif
+	// TODO
+	_cur_palette = pal;
+	return true;
 }
 
-int vga::getch(void)
+bool cga::setpalette(palette::pal_t *pal, int palette_entries)
 {
-#ifdef SDL
-	while (SDL_WaitEvent(&_event)) {
-		/* an event was found */
-		switch (_event.type) {
-		/* close button clicked */
-		case SDL_QUIT:
-			return 0;
-			break;
-
-			/* handle the keyboard */
-		case SDL_KEYDOWN:
-			return _event.key.keysym.sym;
-			break;
-		}
-	}
-
-	return 0;
-#else
-	return getchar();
-#endif
-}
-
-/*
- * SDL routine for implementing input processing loop, since SDL needs to be called a lot of pump events
- */
-int vga::getEvents(int ms)
-{
-#ifdef SDL
-	uint32_t start_time, sched_time;
-
-	start_time=SDL_GetTicks();
-	sched_time=start_time+ms;
-
-	do {
-		if (SDL_PollEvent(&_event)) {
-//		SDL_WaitEventTimeout(&_event,ms);
-		/* an event was found */
-		switch (_event.type) {
-		/* close button clicked */
-		case SDL_QUIT:
-			return -1;
-			break;
-
-			/* handle the keyboard */
-		case SDL_KEYDOWN:
-			return _event.key.keysym.sym;
-			break;
-		default:
-//			return 0;
-			break;
-			}
-		}
-	} while (SDL_GetTicks() < sched_time);
-
-	return 0;
-#else
-	delay(ms);
-	return this->kbhit();
-#endif
+	// TODO
+	return true;
 }
--- cga.h
+++ cga.h
@@ -1,121 +1,46 @@
 /*
- * vga.h
+ * cga.h
  *
  *  Created on: Jan 3, 2019
  *      Author: pedward
  */
 
-#ifndef VGA_H_
-#define VGA_H_
+#ifndef CGA_H_
+#define CGA_H_
 
-#ifndef __BORLANDC__
-#include <SDL2/SDL.h>
-#define SDL
-#define KBHIT display.kbhit
-#else
+#ifdef __BORLANDC__
 #define KBHIT kbhit
-#include <dos.h>
-#include <conio.h>
 #endif
 
 #include "image.h"
+#include "adapter.h"
 
-#define CRTCa 0x3d8
-#define CRTCb 0x3d4
-#define CRTCma 0x3b8
-#define CRTCmb 0x3b4
-#define CRTCmd 0x3ba
-
-class vga {
-
-public:
-enum Vgamode { NONE = 0, TEXT = 0x03, VGAMONO = 0x11, VGAHI = 0x12, VGALO = 0x13, X16 = 0x15, MDA = 0x07, SDLVGALO = 0x16, SDLX16 = 0x17 };
-
-static const unsigned char gtable[];
-static const unsigned char ttable[];
-
-typedef struct {
-Vgamode mode;
-unsigned int x;
-unsigned int y;
-unsigned int bytes;
-unsigned int fp;
-unsigned char planes;
-unsigned char bpp;
-unsigned char colors;
-unsigned int sr;
-unsigned int scale;
-} video_mode;
-
-static const video_mode video_modes[];
-
-canvas screen;
+class cga : public adapter
+{
+protected:
+	static const adapter::video_mode video_modes[];
+	Mode _savedvmode;
 
 protected:
-#ifdef SDL
-	SDL_Window* _window;
-	SDL_Surface* _screen;
-	SDL_Renderer* _renderer;
-	SDL_Surface* _render;
-	SDL_Texture* _texture;
-	SDL_Event _event;
-	unsigned int _sdlscale;
-#endif
-
-ptr_t _buffer;	// primary screen framebuffer
-unsigned int _row_bytes;
-unsigned int _width;
-unsigned int _height;
-uint32_t buf_size;
-unsigned char bpp;
-unsigned char Bpp;
-Vgamode vmode;
-unsigned int SR;
-enum { MONO, COLOR} card;
-palette::pal_type _cur_palette;
-palette::pal_t *_palette;
-int _palette_size;
+	void write_crtc(unsigned int port, unsigned char reg, unsigned char val);
+	bool x16mode(void);
 
 public:
-unsigned char colors;
-static bool SDLonce;
-
-protected:
-int bytes_per_row(void);
-void write_crtc(unsigned int port, unsigned char reg, unsigned char val);
-bool x16mode(void);
-Vgamode getmode(void);
-
-unsigned char planes;
-
-
-public:
-vga(void);
-~vga(void);
-bool setup(void);
-bool setpalette(palette::pal_type pal);
-palette::pal_type getpalette(void) { return _cur_palette; }
-int width(void) { return _width; }
-int height(void) { return _height; }
-bool sdlmode(Vgamode mode);
-bool graphmode(void);
-bool graphmode(Vgamode mode);
-bool textmode(void);
-bool mdamode(void);
-void setpixel(int x, int y, unsigned char visible);
-unsigned char getpixel(int x, int y);
-void cls(void);
-ptr_t allocate_screen_buffer();
-void update(void);
-void vsync(void);
-void writef(int col, int row, int color, char *format, ...);
-void translate(unsigned char far *src);
-bool detectMDA(void);
-bool kbhit(void);
-int getch(void);
-int getEvents(int ms);
-
+	cga(void);
+	~cga(void);
+	bool setup(void);
+	bool setpalette(palette::pal_type pal);
+	bool setpalette(palette::pal_t *pal, int palette_entries);
+	bool graphmode(Mode mode);
+	adapter::Mode getmode(void);
+	bool textmode(void);
+	void setmode(Mode mode);
+	void cls(void);
+	ptr_t allocate_screen_buffer();
+	void update(void);
+	void vsync(void);
+	void translate(unsigned char far *src);
 
 };
 
-#endif /* VGA_H_ */
+#endif /* CGA_H_ */
