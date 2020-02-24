ba473ceb795e35f854e2e80374dd0c1b4c2ff13d
Perry Harrington <pedward@apsoft.com>
2020-02-24 00:15:23 -0800

    Palette updates
    Changed CGA_PAL to TEXT_PAL since that applies to text mode
    Added CGA{0..2}_PAL and CGA{0..2}HI_PAL to represent 4 color palette modes
    Added a B&W palette for 1bpp modes

--- palettes.h
+++ palettes.h
@@ -1,28 +1,53 @@
 #ifndef VGA_PAL_H
 #define VGA_PAL_H
 
-class palette {
+class palette
+{
 public:
-	typedef struct {
+	typedef struct
+	{
 		unsigned char r;
 		unsigned char g;
 		unsigned char b;
 	} pal_t;
 
-	typedef struct {
+	typedef struct
+	{
 		unsigned char r;
 		unsigned char g;
 		unsigned char b;
 		unsigned char a;
 	} pala_t;
 
-	enum pal_type { CGA_PAL, VGA_PAL, RGB_PAL, CUSTOM, NONE = 255 };
+	enum pal_type
+	{
+		TEXT_PAL,
+		CGA0_PAL,
+		CGA1_PAL,
+		CGA2_PAL,
+		CGA0HI_PAL,
+		CGA1HI_PAL,
+		CGA2HI_PAL,
+		BW_PAL,
+		VGA_PAL,
+		RGB_PAL,
+		CUSTOM,
+		NONE = 255
+	};
 
 	static const pal_t vga_pal[];
-	static const pal_t cga_pal[];
+	static const pal_t bw_pal[];
+	static const pal_t text_pal[];
+	static const pal_t cga0_pal[];
+	static const pal_t cga1_pal[];
+	static const pal_t cga2_pal[];
+	static const pal_t cga0hi_pal[];
+	static const pal_t cga1hi_pal[];
+	static const pal_t cga2hi_pal[];
 	static const pal_t rgb8bpp_pal[];
 
-	typedef struct {
+	typedef struct
+	{
 		pal_type set;
 		int palette_entries;
 		const pal_t *pal;
